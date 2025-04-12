754b8355ebd03d4acc395543fe76c1aa91ffa016
coleyoung02 <94935033+coleyoung02@users.noreply.github.com>
2025-04-12 12:08:20 -0700

    trickle implemented

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -3,25 +3,57 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float acceleration;
     [SerializeField] private Rigidbody2D rb;
+    private float velocity;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        velocity = 0f;
+    }
+
+    private float UseAcceleration(bool left)
+    {
+        float acc;
+        if (left)
+        {
+            acc = 2 + velocity / moveSpeed;
+        }
+        else
+        {
+            acc = 2 - velocity / moveSpeed;
+        }
+        return acc * acceleration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVec = Vector2.zero;
+        bool inputPressed = false;
         if (Input.GetKey(KeyCode.A))
         {
-            moveVec += Vector2.left;
+            velocity -= UseAcceleration(true) * Time.deltaTime;
+            inputPressed = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            moveVec += Vector2.right;
+            velocity += UseAcceleration(false) * Time.deltaTime;
+            inputPressed = true;
         }
-        rb.linearVelocity = moveVec * moveSpeed;
+        velocity = Mathf.Clamp(velocity, -moveSpeed, moveSpeed);
+        if (!inputPressed)
+        {
+            if (velocity > 0f)
+            {
+                velocity = Mathf.Max(velocity - Time.deltaTime * acceleration * 3, 0);
+            }
+            else if (velocity < 0f)
+            {
+                velocity = Mathf.Min(velocity + Time.deltaTime * acceleration * 3, 0);
+            }
+        }
+        rb.linearVelocity = new Vector2(velocity, 0);
     }
 }
---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class Rain : MonoBehaviour
+{
+    [SerializeField] private GameObject poisonDrop;
+    [SerializeField] private Transform leftBound;
+    [SerializeField] private Transform rightBound;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float difficultyScale;
+    private float velocity;
+    private float position;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        acceleration = 2f;
+        velocity = 0f;
+        position = leftBound.position.x + (rightBound.position.x - leftBound.position.x) / 4;
+        StartCoroutine(DripRoutine());
+        StartCoroutine(JoinkRoutine(2f / difficultyScale));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += acceleration * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+        transform.position = new Vector3(position, transform.position.y, transform.position.z);
+        if (position > rightBound.position.x)
+        {
+            position = rightBound.position.x;
+            velocity = Mathf.Min(velocity, 0);
+            acceleration = Mathf.Min(acceleration, -UnityEngine.Random.Range(2, 4));
+        }
+        else if (position < leftBound.position.x)
+        {
+            position = leftBound.position.x;
+            velocity = Mathf.Max(velocity, 0);
+            acceleration = Mathf.Max(acceleration, UnityEngine.Random.Range(2, 4));
+        }
+    }
+
+    private IEnumerator DripRoutine()
+    {
+        Instantiate(poisonDrop, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(.25f);
+        StartCoroutine(DripRoutine());
+    }
+
+    private IEnumerator JoinkRoutine(float f)
+    {
+        yield return new WaitForSeconds(f);
+        if (UnityEngine.Random.Range(0, 6) > 2f)
+        {
+            acceleration = UnityEngine.Random.Range(2f * difficultyScale, 5f * difficultyScale) * (UnityEngine.Random.Range(0, 2) * 2 - 1);
+        }
+        else
+        {
+            acceleration = UnityEngine.Random.Range(2f * difficultyScale, 5f * difficultyScale) * -Mathf.Sign(acceleration);
+        }
+        StartCoroutine(JoinkRoutine(UnityEngine.Random.Range(4.5f / difficultyScale, 12f / difficultyScale)));
+    }
+}
