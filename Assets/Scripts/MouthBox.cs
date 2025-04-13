914c2b5ed96bf2fb3bd1f9cbc2f7f6d9d9357ada
baykidrizzle <92473565+baykidrizzle@users.noreply.github.com>
2025-04-13 15:33:27 -0700

    juice

--- Assets/Scripts/MouthBox.cs
+++ Assets/Scripts/MouthBox.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using System.Collections;
 public class MouthBox : MonoBehaviour
 {
 
+    [SerializeField] Animator ani;
     private GameManager gameManager;
 
     private void Start()
@@ -13,7 +14,17 @@ public class MouthBox : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.GetComponent<DropVisuals>().Eat();
+
+        StartCoroutine(bandaid());
         Destroy(collision);
         gameManager.SwallowDrop();
     }
+    IEnumerator bandaid()
+    {
+        ani.Play("New Animation");
+        yield return null;
+        ani.Play("eat");
+    }
 }
+
+
