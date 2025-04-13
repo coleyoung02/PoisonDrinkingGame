d421dfa4adcd7b36234747f907636284feb8a141
baykidrizzle <92473565+baykidrizzle@users.noreply.github.com>
2025-04-13 13:22:08 -0700

    implemented main menu track

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-using static UnityEditor.Rendering.ShadowCascadeGUI;
+//using static UnityEditor.Rendering.ShadowCascadeGUI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -11,9 +12,12 @@ public class AudioManager : MonoBehaviour
 
     [SerializeField] private List<AudioSource> sfxSources;
     [SerializeField] private AudioClip newsNoise;
+    [SerializeField] AudioSource music;
+    [SerializeField] AudioResource mainmenumusic;
+    [SerializeField] AudioResource gameplaymusic;
     private int sfxIndex = 0;
 
-    private void Start()
+    private void Awake()
     {
         if (Instance != null && Instance != this)
         {
@@ -24,9 +28,29 @@ public class AudioManager : MonoBehaviour
             orphansSaved = 0;
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
     }
+    void OnSceneLoaded(Scene s, LoadSceneMode m)
+    {
+        if (s.name == "MainMenu")
+        {
+            if (music.resource != mainmenumusic)
+            {
+                music.resource = mainmenumusic;
+                music.Play();
+            }
 
+        }
+        else
+        {
+            if (music.resource != gameplaymusic)
+            {
+                music.resource = gameplaymusic;
+                music.Play();
+            }
+        }
+    }
     public void PlayAudioClip(AudioClip clip)
     {
         sfxIndex++;
